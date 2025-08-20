cb532dbca554a2ad0698cb4cd5cf8db10ae3ea27
Taylor Lohman <taylorlohman@duck.com>
2025-08-20 10:18:57 -0700

    see homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
+  skip_before_action :authenticate_users!, only: [:index]
+
   def index
-    @available_passes = PassRental.where(status: 'available').limit(6)
   end
 end
