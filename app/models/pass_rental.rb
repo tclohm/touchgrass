0c1653b5b4f8cd504cdd0a761a5318f906ae2efe
Taylor Lohman <taylorlohman@duck.com>
2025-08-20 11:13:14 -0700

    user and pass rental

--- app/models/pass_rental.rb
+++ app/models/pass_rental.rb
@@ -16,6 +16,6 @@ class PassRental < ApplicationRecord
 
   def end_date_after_start_date
     return unless start_date && end_date
-    errors.add(:end_date, "must be after start date")
+    errors.add(:end_date, "must be after start date") if end_date < start_date
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,21 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :pass_rentals, dependent: :destroy
   has_many :bookings, dependent: :destroy
+  has_one_attached :government_id
+
+  validaes :phone_number, presence: true, format: { with: /A\d{10}\z/, message: "must be 10 digits"}
+
+  scope :verified, -> { where(phone_verified: true, government_id_verified: true) }
+
+  def fully_verified?
+    phone_number? && government_id_verified? && government_id.attached?
+  end
+
+  def verification_status
+    return "Not Started" unless phone_number.present?
+    return "Phone Pending" unless phone_verified?
+    return "ID Pending" unless government_id.attached?
+    return "ID Review" unless government_id_verified?
+    "Fully Verified"
+  end
 end
