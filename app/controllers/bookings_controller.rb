cf6f31a1355d88c988e3038ee044f54b8f1a554c
Taylor Lohman <taylorlohman@duck.com>
2025-08-19 20:13:15 -0700

    booking controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,7 +1,15 @@
 class BookingsController < ApplicationController
   def index
+    @bookings = current_user.bookings.includes(:pass_rental)
   end
 
   def create
+    @pass_rental = PassRental.find(params[:pass_rental_id])
+    @booking = current_user.bookings.create(
+      pass_rental: @pass_rental,
+      status: 'pending'
+    )
+
+    redirect_to @pass_rental, notice: 'Booking request sent!'
   end
 end
