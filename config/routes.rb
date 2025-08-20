0e1f090f25cc89d27005baf4384042395d25492d
Taylor Lohman <taylorlohman@duck.com>
2025-08-19 20:54:37 -0700

    config/routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,12 @@
 Rails.application.routes.draw do
-  get "bookings/index"
-  get "bookings/create"
-  get "pass_rentals/index"
-  get "pass_rentals/show"
-  get "pass_rentals/new"
-  get "pass_rentals/create"
-  get "pass_rentals/edit"
-  get "pass_rentals/update"
-  get "pass_rentals/destroy"
-  get "home/index"
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "home#index"
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  resources :pass_rental do
+    resources :bookings, only: [:create]
+  end
+
+  resources :bookings, only: [:index]
+  
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
